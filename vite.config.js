4ed463d4fcf78aede8b85ee69d3048312ebdf59c
0rnot <100481786+0rnot@users.noreply.github.com>
2026-04-04 10:26:28 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ import yaml from '@rollup/plugin-yaml'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/BA-style-homepage/',
   build: {
     assetsInlineLimit: 0,
     minify: 'esbuild',
@@ -78,7 +79,7 @@ export default defineConfig({
     }),
     VitePWA({
       mode: 'production',
-      base: '/',
+      base: '/BA-style-homepage/',
       registerType: 'prompt',
       injectRegister: 'auto',
       workbox: {
